7dcedf682a682772cbf27b590be939cb07d255d7
Luke Shirnia <lukeshirnia@btinternet.com>
2016-11-02 21:28:50 +0000

    Update information.sh

--- information.sh
+++ information.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-function xml_info {
-
 format='--------------------------------------------------------------------------------------------'
 
+function xml_info {
 echo ''
 echo $config
 echo ''
@@ -16,22 +15,13 @@ echo ''
 echo 'Sessions'
 echo '------------------'
 
-if [[ $(xmllint --nocdata --xpath '/config/global/session_save/text()' $config 2>/dev/null ) == "db" ]]; then
-	#######REDIS
-	echo 'Redis_session     : ' $(xmllint --nocdata --xpath '/config/global/redis_session/host' $config 2>/dev/null | sed 's/<host>//g' | sed 's/<[/]host>//g')
-	echo 'Redis_db          : ' $(xmllint --nocdata --xpath '/config/global/redis_session/db' $config 2>/dev/null | sed 's/<db>//g' | sed 's/<[/]db>//g')
-	echo 'Redis_port        : ' $(xmllint --nocdata --xpath '/config/global/redis_session/port' $config 2>/dev/null | sed 's/<port>//g' | sed 's/<[/]port>//g')
-	echo 'Redis_password    : ' $(xmllint --nocdata --xpath '/config/global/redis_session/password' $config 2>/dev/null | sed 's/<password>//g' | sed 's/<[/]password>//g' )
-	echo ''
-elif [[ $(xmllint --nocdata --xpath '/config/global/session_save/text()' $config 2>/dev/null ) == "memcache" ]]; then
-	######memcache
-	echo 'Memcache          : ' $(xmllint --nocdata --xpath '/config/global/session_save/text()' $config 2>/dev/null )
-	echo 'Memcache_path     : ' $(xmllint --nocdata --xpath '/config/global/session_save_path/text()' $config 2>/dev/null )
+echo 'session_save      : ' $(xmllint --nocdata --xpath '/config/global/session_save/text()' $config 2>/dev/null)
 
+if [[ ! $(xmllint --nocdata --xpath '/config/global/session_save_path/text()' $config 2>/dev/nul ) == "" ]]; then
+        echo 'session_save_path : ' $(xmllint --nocdata --xpath '/config/global/session_save_path/text()' $config 2>/dev/null)
 else
-	echo 'session_save      : ' $(xmllint --nocdata --xpath '/config/global/session_save/text()' $config 2>/dev/null)
+echo "cry"
 fi
-
 
 echo ''
 echo 'Cache'
@@ -55,11 +45,41 @@ echo '------------------'
 echo 'FrontName         : ' $(xmllint --nocdata --xpath '/config/admin/routers/adminhtml/args/frontName/text()' $config 2>/dev/null)
 echo $format
 }
+function whichsite {
+number=0
+echo $format
+echo "A - Show config for all sites"
 
+for config in $(locate app/etc/local.xml | grep xml$); do
+         sitearray+=($config);  done
+
+for i in "${sitearray[@]}"
+do
+        echo "$number" - "$i"
+        number=$[number+1]
+done
+echo $format
+read -p "Which option would you like to choose? " answer
+#answer=$[answer-1]
+echo $format
+}
 function config {
-        echo $format
-        for config in $(locate app/etc/local.xml | grep xml$); do xml_info; done
+echo $format
+        case $answer in
+        "A" )
+                for config in $(locate app/etc/local.xml | grep xml$); do xml_info; done
+        ;;
+
+        [0-9])
+                config=${sitearray[$answer]}; xml_info
+        ;;
+        *)
+                echo "Invalid response"
+        ;;
+        esac
 }
 
+
 updatedb
+whichsite
 config
